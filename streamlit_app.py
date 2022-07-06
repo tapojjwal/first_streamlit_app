a079cbb9f3e0d54ff0fd0e920ea7e1371e38f7ba
tapojjwal <90521952+tapojjwal@users.noreply.github.com>
2022-07-06 16:28:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit
 
 streamlit.title("My Parents First Healthy Dinner!")
 streamlit.header("Breakfast Menu")
+streamlit.text("Item 1")
+streamlit.text("Item 2")
+streamlit.text("Item 3")
