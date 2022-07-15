42ef4125344583a28df8f16b5dfe7c0ccb868eab
tapojjwal <90521952+tapojjwal@users.noreply.github.com>
2022-07-15 10:23:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.header('Fruityvice fruit advice!')
 
 #New section to be added here - 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 streamlit.text(fruityvice_response.json())
 
 # Representing JSON in flat tabular format 
