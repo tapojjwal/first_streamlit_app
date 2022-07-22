8867bf2a1ecd3d55fff794e820e93663599c26d9
tapojjwal <90521952+tapojjwal@users.noreply.github.com>
2022-07-22 11:33:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,18 +26,23 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
+
+#Defying a function
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
 #New section to display Fruityvice api response.
 streamlit.header('Fruityvice fruit advice!')
-
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
   streamlit.write('The user entered ', fruit_choice)
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
     
 except URLError as e:
   streamlit.error()
@@ -61,5 +66,6 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('The user entered ', add_my_fruit)
+
 
 
